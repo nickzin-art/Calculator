13f90b8ef58636fd520f4dc2a56107b7046b531a
nickzin-art <nicolasbez2009.11@gmail.com>
2025-11-26 10:56:48 -0300

    Feat:: metodo add Number
    O metodo de Adicionar os numeros e a virgula na calculadora

---
+++ funcoes.py
@@ -0,0 +1,17 @@
+def sum(number1, number2):
+    result = number1 + number2
+    return result
+
+def sub(number1, number2):
+    result = number1 - number2
+    return result
+
+def multiplicar(number1, number2):
+    result = number1 * number2
+    return result
+
+def div(number1, number2):
+    if number2 == 0:
+        return "Erro na divisão"
+    return number1 / number2
+
