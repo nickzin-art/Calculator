794ec54f323c7586c6a96196210d81bda805b14f
nickzin-art <nicolasbez2009.11@gmail.com>
2025-12-03 09:11:35 -0300

    commit calculadora semi funcional

--- funcoes.py
+++ funcoes.py
@@ -1,17 +1,17 @@
-def sum(number1, number2):
-    result = number1 + number2
+def sum(num1, num2):
+    result = num1 + num2
     return result
 
-def sub(number1, number2):
-    result = number1 - number2
+def sub(num1, num2):
+    result = num1 - num2
     return result
 
-def multiplicar(number1, number2):
-    result = number1 * number2
+def multiplicar(num1, num2):
+    result = num1 * num2
     return result
 
-def div(number1, number2):
-    if number2 == 0:
+def div(num1, num2):
+    if num2 == 0:
         return "Erro na divisão"
-    return number1 / number2
+    return num1 / num2
 
--- view/Calc_Tela.py
+++ view/Calc_Tela.py
@@ -1,69 +1,114 @@
-from PyQt5.QtWidgets import QLCDNumber
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QLCDNumber, QApplication, QMainWindow
 from PyQt5.uic import loadUi
-from PyQt5.QtGui import QKeySequence
+from funcoes import sum, sub, multiplicar, div
 
 class TelaPrograma(QMainWindow):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         loadUi("view/untitled.ui", self)
         self.show()
-        
-        self.lcdNumber = QLCDNumber()
 
-        
-        self.btn_1.clicked.connect(lambda: self.AddNumber(1))
-        self.btn_2.clicked.connect(lambda: self.AddNumber(2))
-        self.btn_3.clicked.connect(lambda: self.AddNumber(3))
-        self.btn_4.clicked.connect(lambda: self.AddNumber(4))
-        self.btn_5.clicked.connect(lambda: self.AddNumber(5))
-        self.btn_6.clicked.connect(lambda: self.AddNumber(6))
-        self.btn_7.clicked.connect(lambda: self.AddNumber(7))
-        self.btn_8.clicked.connect(lambda: self.AddNumber(8))
-        self.btn_9.clicked.connect(lambda: self.AddNumber(9))
-        
+        self.current_value = ""
+        self.last_value = ""
+        self.operation = None
 
-class Calculadora(QMainWindow):
-    
-    def addNumber(self, number):
-        numberDisplay1 = self.display.setText(number)
-        self.display_2.setText(numberDisplay1)
-        
-        
-    def cleanDisplay(self):
-        self.display.setText("0")
-        
-    def setOperation(self):
-        result = self.display.text()
-        self.display_2.setText(result)
-        
-    def getNumberDisplay(self, display):
-        num = display.text()
-        if "," in num1:
-            num1 = num1.replace(',', '.')
-            num1 = float(num1)
-        num1 = int(num1) 
-        
-    def setNumberDisplay(self, number):
-        number = str(number)
-        number = number.replace('.', ',')
-        self.display.setText(number)
-        
-    def showResult(self):
-        num1 = self.getNumberDisplay(self.display)
-        num2 = self.getNumberDisplay(self.display_2)
-        
-        result = sum(num1, num2)
-        self.setNumberDisplay(result)
-        
-    def addComma(self):
-        last = self.display.text()
-        if last.count(",") > 0:
-            result = last
+        self.btn_0.clicked.connect(lambda: self.add_digito("0"))
+        self.btn_1.clicked.connect(lambda: self.add_digito("1"))
+        self.btn_2.clicked.connect(lambda: self.add_digito("2"))
+        self.btn_3.clicked.connect(lambda: self.add_digito("3"))
+        self.btn_4.clicked.connect(lambda: self.add_digito("4"))
+        self.btn_5.clicked.connect(lambda: self.add_digito("5"))
+        self.btn_6.clicked.connect(lambda: self.add_digito("6"))
+        self.btn_7.clicked.connect(lambda: self.add_digito("7"))
+        self.btn_8.clicked.connect(lambda: self.add_digito("8"))
+        self.btn_9.clicked.connect(lambda: self.add_digito("9"))
+
+        self.btn_add.clicked.connect(lambda: self.set_operation("+"))
+        self.btn_sub.clicked.connect(lambda: self.set_operation("-"))
+        self.btn_mul.clicked.connect(lambda: self.set_operation("*"))
+        self.btn_div.clicked.connect(lambda: self.set_operation("/"))
+
+        self.btn_equal.clicked.connect(self.equals)
+        self.btn_clear.clicked.connect(self.clear_all)
+        self.btn_comma.clicked.connect(self.add_comma)
+        self.btn_back.clicked.connect(self.backspace)
+
+    def atualizar_display(self):
+        if self.current_value == "":
+            self.lcdMain.display("0")
         else:
-            result = last + ","
-        self.display.setText(result)
-        
-    
+            self.lcdMain.display(self.current_value)
 
+    def atualizar_historico(self):
+        if self.last_value and self.operation:
+            self.lcdHistory.display(f"{self.last_value} {self.operation}")
+        else:
+            self.lcdHistory.display("")
 
+    def add_digito(self, digit):
+        if self.current_value == "0":
+            self.current_value = digit
+        else:
+            self.current_value += digit
+        self.atualizar_display()
+
+    def add_comma(self):
+        if "," not in self.current_value:
+            if self.current_value == "":
+                self.current_value = "0,"
+            else:
+                self.current_value += ","
+        self.atualizar_display()
+
+    def backspace(self):
+        self.current_value = self.current_value[:-1]
+        self.atualizar_display()
+
+    def clear_all(self):
+        self.current_value = ""
+        self.last_value = ""
+        self.operation = None
+        self.atualizar_display()
+        self.atualizar_historico()
+
+    def set_operation(self, op):
+        if self.current_value == "":
+            return
+        self.last_value = self.current_value.replace(",", ".")
+        self.current_value = ""
+        self.operation = op
+        self.atualizar_historico()
+        self.atualizar_display()
+
+    def equals(self):
+        if not self.operation or self.current_value == "":
+            return
+
+        num1 = float(self.last_value)
+        num2 = float(self.current_value.replace(",", "."))
+
+        if self.operation == "+":
+            result = sum(num1, num2)
+        elif self.operation == "-":
+            result = sub(num1, num2)
+        elif self.operation == "*":
+            result = multiplicar(num1, num2)
+        elif self.operation == "/":
+            result = div(num1, num2)
+            if result == "Erro na divisão":
+                self.lcdMain.display("Erro")
+                return
+        else:
+            return
+
+        self.lcdHistory.display(f"{num1} {self.operation} {num2}")
+        self.last_value = ""
+        self.operation = None
+        self.current_value = str(result).replace(".", ",")
+        self.atualizar_display()
+
+if __name__ == "__main__":
+    import sys
+    app = QApplication(sys.argv)
+    window = TelaPrograma()
+    sys.exit(app.exec_())
